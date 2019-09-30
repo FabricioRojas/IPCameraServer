a67b35541a740fdf2720b84b7c9ec55c0dfc0024
Fabricio Rojas <fabriciorojascuomo@outlook.es>
2019-09-30 19:00:44 +0100

    Proyect upload

--- server/main.js
+++ server/main.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var fs = require("fs");
+
 var app = express();
-var server = require('http').Server(app);
+// var server = require('http').Server(app);
 var io = require('socket.io')(server);
+const dgram = require('dgram');
+var server = dgram.createSocket('udp4');
 
 var messages = [];
 
@@ -11,22 +15,41 @@ app.get('/', function(req, res) {
   res.status(200).send("Server running");
 });
 
-io.on('connection', function(socket) {
-  console.log('Alguien se ha conectado con Sockets');
-  socket.emit('messages', messages);
+// io.on('connection', function(socket) {
+//   console.log('Alguien se ha conectado con Sockets');
+//   socket.emit('messages', messages);
 
-  socket.on('new-message', function(data) {
-    messages.push(data);
+//   socket.on('new-message', function(data) {
+//     messages.push(data);
 
-    io.sockets.emit('messages', messages);
+//     io.sockets.emit('messages', messages);
+//   });
+// });
+
+
+// io.on('newFrame', function(socket) {
+// 	console.log('New frame received');
+// });
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  //console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  fs.writeFile("out.png", msg, 'base64', function(err) {
+    if(err) console.log("Error", err);
+    console.log("r", new Date());
   });
 });
 
-
-io.on('newFrame', function(socket) {
-	console.log('New frame received');
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.listen(8080, function() {
-  console.log("Servidor corriendo en http://localhost:8080");
-});
+server.bind(41234);
+
+// server.listen(8080, function() {
+//   console.log("Servidor corriendo en http://localhost:8080");
+// });
